45ae667fc7350dc2ba1337bc0868e8dac6e687c1
andrebbk <andre.f.m.silva@hotmail.com>
2025-05-13 12:23:22 +0100

    Updated to angular 19

--- BssDesktopApp/src/app/app.component.ts
+++ BssDesktopApp/src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component, NgZone, OnInit } from '@angular/core';
 import { WindowRefServiceService } from './services/window-ref-service.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    standalone: false
 })
 export class AppComponent implements OnInit{
   title = 'BssDesktopApp';
